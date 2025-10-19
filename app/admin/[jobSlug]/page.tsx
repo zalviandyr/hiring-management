dd6c6e9bed579b3a999f58882233568715cea1f6
Zukron Alviandy <zukronalviandy@gmail.com>
2025-10-20 02:00:45 +0700

    feat: load candidate

--- app/admin/[jobSlug]/page.tsx
+++ app/admin/[jobSlug]/page.tsx
@@ -4,17 +4,26 @@ import { useParams } from "next/navigation";
 import { CandidateEmpty } from "./_components/CandidateEmpty";
 import { useCandidates } from "@/features/candidates/queries/use-candidates";
 import { ApplicantTable } from "./_components/ApplicantTable";
+import { Loader2 } from "lucide-react";
 
 const AdminManageJobPage = () => {
   const { jobSlug } = useParams<{ jobSlug?: string }>();
-  const { data } = useCandidates(jobSlug);
+  const { data, isPending } = useCandidates(jobSlug);
 
   return (
     <div className="flex flex-col gap-6">
       <span className="text-lg font-bold">{jobSlug}</span>
 
       <div className="border border-neutral-40 rounded-lg p-6">
-        {Boolean(data) ? <ApplicantTable data={data ?? []} /> : <CandidateEmpty />}
+        {isPending ? (
+          <div className="flex justify-center">
+            <Loader2 className="size-4 animate-spin" />
+          </div>
+        ) : data?.length ?? 0 > 0 ? (
+          <ApplicantTable data={data ?? []} />
+        ) : (
+          <CandidateEmpty />
+        )}
       </div>
     </div>
   );
--- features/candidates/queries/use-candidates.ts
+++ features/candidates/queries/use-candidates.ts
@@ -2,17 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getCandidates } from "../api/get-candidates";
 import { ApplicantFormData } from "@/features/applicants/schema";
 
-export const useCandidatesKey = ["candidates"];
+export const useCandidatesKey = (jobSlug: string | undefined) => ["candidates", jobSlug] as const;
 
 export const useCandidates = (jobSlug?: string) =>
   useQuery<ApplicantFormData[]>({
-    queryKey: useCandidatesKey,
-    queryFn: async () => {
-      if (jobSlug) {
-        return await getCandidates(jobSlug);
-      }
-
-      return [];
+    queryKey: useCandidatesKey(jobSlug),
+    queryFn: () => {
+      if (!jobSlug) return [];
+      return getCandidates(jobSlug);
     },
     enabled: Boolean(jobSlug),
   });
